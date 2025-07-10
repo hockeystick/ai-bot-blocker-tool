096b4d75b473daf6ae68f4b88db70f7781426db4
Ali Mahmood <alimahmood@Alis-Mac-mini.local>
2025-07-10 22:56:14 +0200

    fix: configure Tailwind CSS (install, configs, globals.css)

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    // add other folders if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
